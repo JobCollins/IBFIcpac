8c3def6248a2c8c0fb9866897551a59143c4ee8f
JobCollins <dulo.collins@gmail.com>
2021-11-17 15:55:13 +0300

    Feature: Map Created the base map in dark mode

--- ibfMap/ibf-map/src/App.js
+++ ibfMap/ibf-map/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react"
+import mapboxgl from "mapbox-gl"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
+
+const App = () => {
+  const mapContainer = useRef()
+
+  // this is where all of our map logic is going to live
+  // adding the empty dependency array ensures that the map
+  // is only rendered once
+  useEffect(() => {
+    //create the map and configure it
+    //checkout the API reference for options
+    //https://docs.mapbox.com/mapbox-gl-js/api/map
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/dark-v10",
+      center:[38, -1],
+      zoom: 12,
+    })
+    //clean up function to remove map on unmount
+    return () => map.remove()
+  }, [])
+  return <div ref={mapContainer} style={{width: "100%", height: "100vh"}}></div>
 }
 
-export default App;
+export default App
